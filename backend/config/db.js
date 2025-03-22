9e60a33421cca53df2ae5e5c3e54383ee5967b44
Nicol Melgarejo <niccolly.melgarejo@tecsup.edu.pe>
2025-03-22 11:45:04 -0500

    Subiendo CAMBIOS

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,9 +4,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'system',
+    host: process.env.DB_HOST || 'p3plzcpnl507573.prod.phx3.secureserver.net',
+    user: process.env.DB_USER || 'l74786401iit',
+    password: process.env.DB_PASSWORD || 'ExpertRubber2002$',
     database: process.env.DB_NAME || 'crud_react'
 });
 
